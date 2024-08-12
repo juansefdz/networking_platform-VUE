6ceb2de7ecb3a363f3635e26cbcac0bd8fbe2132
juansefdz <jsfm81091@gmail.com>
2024-08-12 13:54:41 -0500

    update team zone

--- src/components/TeamZone/Team.vue
+++ src/components/TeamZone/Team.vue
@@ -2,55 +2,38 @@
   <div class="team-container">
     <TeamCard
       v-for="team in teams"
-      :key="team.id" 
+      :key="team.id"
       :title="team.title"
       :description="team.description"
-      :profiles="team.profiles"
+      :profiles="team.team"
       :technologies="team.technologies"
+      :logo="team.logo"
     />
   </div>
 </template>
 
 <script>
-import TeamCard from './TeamCard.vue';
+import TeamCard from "./TeamCard.vue";
+import teamsInfo from "@/assets/data/TeamZone/teams.json";
 
 export default {
   components: {
-    TeamCard
+    TeamCard,
   },
   data() {
     return {
-      teams: []
-    }
+      teams: teamsInfo,
+    };
   },
-  created(){
-    this.loadTeams();
-  },
-  methods: {
-      async loadTeams(){
-        try{
-          const response = await fetch('/assets/db/teams.json');
-          const data = await response.json();
-          this.teams = data;
-        } catch(error){
-          console.error("error loading teams",error);
-        }
-      }
-    
-  }
-}
+};
 </script>
 
 <style>
-   .team-container{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    width: 70vw;
-    height: 100vh;
-    background-color: red;
-
-  }
-
- 
+.team-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 70vw;
+  height: auto;
+}
 </style>
--- src/components/TeamZone/TeamCard.vue
+++ src/components/TeamZone/TeamCard.vue
@@ -1,26 +1,35 @@
 <template>
-    <div class="team-card">
-      <div class="team-card-title">
-        <!-- Agregar logo -->
-        <p>{{ title }}</p>
-        <div class="card-title-actions">
-          <i class='bx bx-star'></i>
-          <i class='bx bx-dots-vertical'></i>
-        </div>
+  <div class="team-card">
+    <div class="team-card-title">
+      <!-- Logo del equipo -->
+      <img :src="logo" alt="Team Logo" class="team-logo" />
+      <p>{{ title }}</p>
+      <div class="card-title-actions">
+        <i class="bx bx-star"></i>
+        <i class="bx bx-dots-vertical"></i>
       </div>
-      <div class="team-card-content">
-        <p>{{ description }}</p>
-        <div class="team-users">
-          <div class="profiles-zone">
-          
-          </div>
-          <div class="technology-zone">
-            <span v-for="tech in technologies" :key="tech">{{ tech }}</span>
-          </div>
+    </div>
+    <div class="team-card-content">
+      <p>{{ description }}</p>
+      <div class="team-users">
+        <div class="profiles-zone">
+          <img
+            v-for="(member, index) in profiles"
+            :key="index"
+            :src="member"
+            alt="Team Member"
+            class="team-profile-image"
+          />
+        </div>
+        <div class="technology-zone">
+          <span v-for="(tech, index) in technologies" :key="index">{{
+            tech
+          }}</span>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
 export default {
@@ -28,43 +37,76 @@ export default {
     title: String,
     description: String,
     profiles: Array,
-    technologies: Array
-  }
-}
+    technologies: Array,
+    logo: String,
+  },
+};
 </script>
 
 <style scoped>
- .team-card{
-
-width: 400px;
-height: 250px;
-background-color: blue;
-border-radius: 10px;
-margin: 20px;
+.team-card {
+  width: 300px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 5px;
+  margin: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
-.team-card-title{
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 10px;
-background-color: #f2f5fa;
-border-radius: 10px 10px 0 0;
-font-weight: 200;
-}
-.team-card
-.card-title-actions{
-display: flex;
-gap: 10px;
+.team-card-title {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #ffffff;
+  border-radius: 10px 10px 0 0;
+  font-weight: 200;
 }
 
-.team-card-content{
-padding: 10px;
+.team-card-title p {
+  font-size: 17px;
+  font-weight: 500;
+  margin-left: 10px;
 }
 
+.team-logo {
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+  margin-right: 10px;
+}
+
+.card-title-actions {
+  display: flex;
+  gap: 10px;
+  margin-left: auto;
+}
+
+.team-card-content {
+  padding: 10px;
+}
+
+.team-card-content p {
+  font-size: 14px;
+  font-weight: 200;
+}
+
+.profiles-zone {
+  display: flex;
+  gap: 10px;
+  height: 100px;
+}
+
+.team-profile-image {
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+.technology-zone {
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  font-size: 10px;
+  justify-content: end;
+}
 </style>
-
-
-  
-    
-  
